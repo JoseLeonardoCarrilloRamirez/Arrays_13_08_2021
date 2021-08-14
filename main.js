c625b38c41f52ce5393289e583259e3dab0b4cb4
JoseLeonardoCarrilloRamirez <joseleonardocarrillo@gmail.com>
2021-08-13 21:10:19 -0500

    arrays_copyWithing

---
+++ main.js
@@ -0,0 +1,30 @@
+let nombres = [`Jose`, `Felipe`, `William`, `Leonardo`, `Miguel`, `Andres`];
+
+console.log(nombres);
+
+console.log(nombres.copyWithin(3,0));
+
+console.log(nombres.copyWithin(2,0,2));
+
+console.log(nombres.copyWithin(2,0,-3));
+
+
+// let generar = ()=> Math.trunc(Math.random()*10);
+// let lista = new Array(20).fill(null);
+// for(let i in lista){
+//     lista[i] = generar();
+// }
+// lista = ["A","B","C","D","E","F"];
+// lista.splice(-1,0,"hola")
+// console.log(lista);
+
+
+
+
+
+
+
+// console.log(lista);
+// console.log(lista.slice(1,-2));
+
+// console.log(-(lista.length-1));
